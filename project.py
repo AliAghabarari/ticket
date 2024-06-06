c885958be6b90b0e20eda4436c66c7c5793e934f
AliAghabarari <aliaghabarari213@gmail.com>
2024-06-06 16:19:27 +0330

    Update project.py

--- project.py
+++ project.py
@@ -3,6 +3,8 @@ import sqlite3
 import os
 from tkinter import messagebox, IntVar, ttk
 
+import smtplib, requests
+
 '''
 https://7learn.com/blog/tkinter-in-python
 
@@ -16,6 +18,7 @@ class Ceremony_data:
         self.path = path
         self.con, self.cur = self.connector()
         self.create = self.create_table()
+
     
     def connector(self):
 
@@ -25,7 +28,7 @@ class Ceremony_data:
     
     def create_table(self):
 
-        self.cur.execute('CREATE TABLE IF NOT EXISTS Ceremony(Ceremony_name TEXT PRIMARY KEY, Address TEXT, Date TEXT)')
+        self.cur.execute('CREATE TABLE IF NOT EXISTS Ceremony(Ceremony_name TEXT PRIMARY KEY, Address TEXT, Date TEXT, Description TEXT)')
         self.con.commit()
     
     def insert_data(self, Ceremony_name, Address, Date):
@@ -33,6 +36,24 @@ class Ceremony_data:
         self.cur.execute('INSERT INTO Ceremony VALUES(?, ?, ?)', (Ceremony_name, Address, Date))
         self.con.commit()
     
-    def update_data_ceremony_name(self, new_name):
+    def update_data_ceremony_name(self, ceremony_name, new_name):
 
-        self.cur.execute('UPDATE Ceremony SET Ceremony_name = ?')
+        self.cur.execute('UPDATE Ceremony SET Ceremony_name = ? WHERE Ceremony_name = ?', (new_name, ceremony_name))
+        self.con.commit()
+
+    def update_data_ceremony_address(self, ceremony_name,new_address):
+
+        self.cur.execute('UPDATE Ceremony SET Address = ? WHERE Ceremony_name = ?', (new_address, ceremony_name))
+        self.con.commit()
+
+    def update_data_ceremony_date(self, ceremony_name,new_date):
+
+        self.cur.execute('UPDATE Ceremony SET Date = ? WHERE Ceremony_name = ?', (new_date, ceremony_name))
+        self.con.commit() 
+
+    def update_data_ceremony_description(self, ceremony_name,new_description):
+
+        self.cur.execute('UPDATE Ceremony SET Description = ? WHERE Ceremony_name = ?', (new_description, ceremony_name))
+        self.con.commit()
+
+
